c65a9c7e5c21a58533608b0eba46cd6b8a6e6999
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-05-01 12:16:53 +0300

    rhw6

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { filtersReducer } from "./filtersSlice";
 
@@ -16,6 +25,12 @@ export const store = configureStore({
     contacts: persistedReducer,
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
